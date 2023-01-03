7860ed254eb88bae1c596c0be0ff2cb3ce583bec
William Baele <baelewilliam1999@gmail.com>
2023-01-03 11:22:56 +0100

    flash messages fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   add_flash_types :info, :error, :success
+
+  def after_sign_in_path_for(resource)
+    flash[:success] = "Signed in"
+    root_path
+  end
 end
--- app/controllers/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-class SessionsController < ApplicationController
-  def create
-    # Code de connexion de l'utilisateur
-    flash[:success] = "Signed in"
-    redirect_to root_path
-  end
-end
