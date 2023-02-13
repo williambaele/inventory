98bfd4f0958c1a8fe8ed0f94e70bbd7a5419fc7e
William Baele <baelewilliam1999@gmail.com>
2023-02-13 10:19:33 +0100

    Small changes on index controller'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,12 @@ class PagesController < ApplicationController
   def index
     @items = Item.all
     @sales = Sale.all
+    @profit = 0
+    @sales_past = Sale.where("sale_date >= ?", Time.now - 30.days)
+    @sales_past.each do |sale|
+      if sale.item.retail.present?
+        @profit += sale.sale_price - sale.item.retail
+      end
+    end
   end
 end
